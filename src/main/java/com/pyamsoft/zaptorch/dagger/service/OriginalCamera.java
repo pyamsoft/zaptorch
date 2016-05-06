3c2927332e6ee2a7b126d9cd973793a90f8e0089
pyamsoft <pyam.soft@gmail.com>
2016-05-06 09:32:42 -0700

    Migrate original camera to rx

--- src/main/java/com/pyamsoft/zaptorch/dagger/service/OriginalCamera.java
+++ src/main/java/com/pyamsoft/zaptorch/dagger/service/OriginalCamera.java
@@ -19,7 +19,6 @@ package com.pyamsoft.zaptorch.dagger.service;
 import android.content.Context;
 import android.graphics.PixelFormat;
 import android.hardware.Camera;
-import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.view.Gravity;
 import android.view.SurfaceHolder;
@@ -27,20 +26,25 @@ import android.view.SurfaceView;
 import android.view.WindowManager;
 import com.pyamsoft.zaptorch.app.service.VolumeServicePresenter;
 import java.io.IOException;
-import java.lang.ref.WeakReference;
 import java.util.List;
+import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+import rx.subscriptions.Subscriptions;
 import timber.log.Timber;
 
 @SuppressWarnings("deprecation") final class OriginalCamera extends CameraCommon
     implements SurfaceHolder.Callback {
 
-  private final WindowManager windowManager;
-  private final SurfaceView surfaceView;
-  private final WindowManager.LayoutParams params;
+  @NonNull private final WindowManager windowManager;
+  @NonNull private final SurfaceView surfaceView;
+  @NonNull private final WindowManager.LayoutParams params;
+
+  @NonNull private Subscription cameraSubscription = Subscriptions.empty();
 
   private Camera camera;
   private boolean opened;
-  private CameraAsyncTask cameraAsyncTask;
 
   public OriginalCamera(final @NonNull Context context,
       final @NonNull VolumeServicePresenter presenter) {
@@ -62,24 +66,16 @@ import timber.log.Timber;
         | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
   }
 
-  private void setOpened(boolean opened) {
-    Timber.d("Set opened: %s", opened);
-    this.opened = opened;
-  }
-
-  private void setCamera(Camera camera) {
-    Timber.d("Set camera field to: %s", camera);
-    this.camera = camera;
-  }
-
-  private void addSurface() {
-    windowManager.addView(surfaceView, params);
-  }
-
   private SurfaceHolder getInitializedHolder() {
     final SurfaceHolder holder = surfaceView.getHolder();
     holder.addCallback(this);
     return holder;
+  }
+
+  private void unsubCameraSubscription() {
+    if (!cameraSubscription.isUnsubscribed()) {
+      cameraSubscription.unsubscribe();
+    }
   }
 
   @Override public void toggleTorch() {
@@ -88,11 +84,58 @@ import timber.log.Timber;
       release();
     } else {
       Timber.d("Camera is closed, open it");
-      if (cameraAsyncTask != null) {
-        cameraAsyncTask.cancel(true);
-      }
-      cameraAsyncTask = new CameraAsyncTask(this);
-      cameraAsyncTask.execute();
+      unsubCameraSubscription();
+      cameraSubscription = Observable.defer(() -> Observable.just(Camera.open()))
+          .filter(camera1 -> camera1 != null)
+          .filter(camera1 -> {
+            final Camera.Parameters parameters = camera1.getParameters();
+            if (parameters.getFlashMode() == null) {
+              Timber.e("Null flash mode");
+              camera1.release();
+              return false;
+            }
+
+            final List<String> supportedFlashModes = parameters.getSupportedFlashModes();
+            if (supportedFlashModes == null || supportedFlashModes.isEmpty() || !supportedFlashModes
+                .contains(Camera.Parameters.FLASH_MODE_TORCH)) {
+              Timber.e("Camera parameters do not include Torch");
+              camera1.release();
+              return false;
+            }
+
+            Timber.d("Camera should have torch mode");
+            return true;
+          })
+          .subscribeOn(Schedulers.io())
+          .observeOn(AndroidSchedulers.mainThread())
+          .subscribe(camera1 -> {
+            try {
+              Timber.d("Camera has flash");
+              camera = camera1;
+              windowManager.addView(surfaceView, params);
+
+              Timber.d("set preview");
+              final SurfaceHolder holder = getInitializedHolder();
+              camera.setPreviewDisplay(holder);
+              final Camera.Parameters cameraParameters = camera.getParameters();
+              cameraParameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+              camera.setParameters(cameraParameters);
+
+              Timber.d("start camera");
+              camera.startPreview();
+              opened = true;
+            } catch (IOException e) {
+              throw new CameraSetupError();
+            }
+          }, throwable -> {
+            Timber.e(throwable, "Error opening camera");
+            if (camera != null) {
+              camera.release();
+              camera = null;
+              opened = false;
+            }
+            startErrorExplanationActivity();
+          });
     }
   }
 
@@ -108,11 +151,7 @@ import timber.log.Timber;
       camera = null;
       opened = false;
     }
-
-    if (cameraAsyncTask != null) {
-      cameraAsyncTask.cancel(true);
-      cameraAsyncTask = null;
-    }
+    unsubCameraSubscription();
 
     windowManager.removeView(surfaceView);
   }
@@ -139,86 +178,7 @@ import timber.log.Timber;
     }
   }
 
-  static final class CameraAsyncTask extends AsyncTask<Void, Void, Camera> {
+  static final class CameraSetupError extends RuntimeException {
 
-    private final WeakReference<OriginalCamera> cameraWeakReference;
-
-    CameraAsyncTask(final @NonNull OriginalCamera originalCamera) {
-      this.cameraWeakReference = new WeakReference<>(originalCamera);
-    }
-
-    private static boolean hasFlash(final Camera camera) {
-      if (camera == null) {
-        Timber.e("Null camera");
-        return false;
-      }
-
-      final Camera.Parameters parameters = camera.getParameters();
-      if (parameters.getFlashMode() == null) {
-        Timber.e("Null flash mode");
-        return false;
-      }
-
-      final List<String> supportedFlashModes = parameters.getSupportedFlashModes();
-      if (supportedFlashModes == null
-          || supportedFlashModes.isEmpty()
-          || !supportedFlashModes.contains(Camera.Parameters.FLASH_MODE_TORCH)) {
-        Timber.e("Camera parameters do not include Torch");
-        return false;
-      }
-
-      Timber.d("Camera should have torch mode");
-      return true;
-    }
-
-    @Override protected Camera doInBackground(Void... voids) {
-      return Camera.open();
-    }
-
-    @Override protected void onPostExecute(Camera camera) {
-      super.onPostExecute(camera);
-      final OriginalCamera originalCamera = cameraWeakReference.get();
-      if (originalCamera == null) {
-        Timber.e("Camera weakRef is NULL");
-        return;
-      }
-
-      try {
-        Timber.d("Check if camera has flash");
-        if (hasFlash(camera)) {
-          Timber.d("Camera has flash");
-          originalCamera.setCamera(camera);
-          originalCamera.addSurface();
-          Timber.d("set preview");
-          final SurfaceHolder holder = originalCamera.getInitializedHolder();
-          camera.setPreviewDisplay(holder);
-          final Camera.Parameters params1 = camera.getParameters();
-          params1.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-          camera.setParameters(params1);
-          Timber.d("start camera");
-          camera.startPreview();
-          originalCamera.setOpened(true);
-        } else {
-          Timber.e("Camera does not have flash");
-          camera.release();
-          originalCamera.setCamera(null);
-          originalCamera.setOpened(false);
-        }
-      } catch (Exception e) {
-        Timber.e(e, "toggleTorch onError");
-        originalCamera.setCamera(null);
-        originalCamera.setOpened(false);
-        originalCamera.startErrorExplanationActivity();
-      }
-    }
-
-    @Override protected void onCancelled(Camera camera) {
-      Timber.e("CameraAsyncTask has been cancelled");
-      final OriginalCamera originalCamera = cameraWeakReference.get();
-      if (originalCamera != null) {
-        originalCamera.setCamera(null);
-        originalCamera.setOpened(false);
-      }
-    }
   }
 }
