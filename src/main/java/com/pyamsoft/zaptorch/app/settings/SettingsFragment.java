a6e78deb58ba4a9f2dd426c82a05d1252556825d
pyamsoft <pyam.soft@gmail.com>
2016-12-04 22:27:13 -0800

    Update databinding

--- src/main/java/com/pyamsoft/zaptorch/app/settings/SettingsFragment.java
+++ src/main/java/com/pyamsoft/zaptorch/app/settings/SettingsFragment.java
@@ -16,7 +16,6 @@
 
 package com.pyamsoft.zaptorch.app.settings;
 
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -62,7 +61,7 @@ public class SettingsFragment extends ActionBarFragment implements SettingsFragm
   @Nullable @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
-    binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false);
+    binding = FragmentMainBinding.inflate(inflater, container, false);
     return binding.getRoot();
   }
 
