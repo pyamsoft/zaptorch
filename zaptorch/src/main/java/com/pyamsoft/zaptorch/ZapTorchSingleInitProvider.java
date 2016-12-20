9cb935d0bec78024d6a827393b70d560ab082de1
pyamsoft <pyam.soft@gmail.com>
2016-12-19 22:24:10 -0800

    Convert from module to full project

---
+++ zaptorch/src/main/java/com/pyamsoft/zaptorch/ZapTorchSingleInitProvider.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2016 Peter Kenji Yamanaka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pyamsoft.zaptorch;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import com.pyamsoft.pydroid.BuildConfigChecker;
+import com.pyamsoft.pydroid.IPYDroidApp;
+import com.pyamsoft.pydroid.SingleInitContentProvider;
+import com.pyamsoft.zaptorch.dagger.ZapTorchModule;
+
+public class ZapTorchSingleInitProvider extends SingleInitContentProvider
+    implements IPYDroidApp<ZapTorchModule> {
+
+  @Nullable private ZapTorchModule module;
+
+  @NonNull @Override protected BuildConfigChecker initializeBuildConfigChecker() {
+    return new BuildConfigChecker() {
+      @Override public boolean isDebugMode() {
+        return BuildConfig.DEBUG;
+      }
+    };
+  }
+
+  @Override protected void onInstanceCreated(@NonNull Context context) {
+    Injector.set(module);
+  }
+
+  @Override protected void onFirstCreate(@NonNull Context context) {
+    super.onFirstCreate(context);
+    module = new ZapTorchModule(context);
+  }
+
+  @NonNull @Override public ZapTorchModule provideComponent() {
+    if (module == null) {
+      throw new NullPointerException("ZapTorchComponent is NULL");
+    }
+    return module;
+  }
+
+  @Nullable @Override public String provideGoogleOpenSourceLicenses(@NonNull Context context) {
+    return null;
+  }
+
+  @Override public void insertCustomLicensesIntoMap() {
+
+  }
+}
---
+++ zaptorch/src/main/java/com/pyamsoft/zaptorch/app/settings/SettingsFragment.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2016 Peter Kenji Yamanaka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pyamsoft.zaptorch.app.settings;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.pyamsoft.pydroid.app.PersistLoader;
+import com.pyamsoft.pydroid.tool.AsyncDrawable;
+import com.pyamsoft.pydroid.tool.AsyncMap;
+import com.pyamsoft.pydroid.tool.AsyncMapHelper;
+import com.pyamsoft.pydroid.util.AppUtil;
+import com.pyamsoft.pydroid.util.PersistentCache;
+import com.pyamsoft.pydroiddesign.fab.HideScrollFABBehavior;
+import com.pyamsoft.pydroiddesign.util.FABUtil;
+import com.pyamsoft.pydroidui.app.fragment.ActionBarFragment;
+import com.pyamsoft.zaptorch.R;
+import com.pyamsoft.zaptorch.ZapTorch;
+import com.pyamsoft.zaptorch.databinding.FragmentMainBinding;
+
+public class SettingsFragment extends ActionBarFragment implements SettingsFragmentPresenter.View {
+
+  @NonNull public static final String TAG = "MainSettingsFragment";
+  @NonNull private static final String KEY_PRESENTER = "key_settings_presenter";
+  @SuppressWarnings("WeakerAccess") SettingsFragmentPresenter presenter;
+  private FragmentMainBinding binding;
+  private long loadedKey;
+  @Nullable private AsyncMap.Entry fabTask;
+
+  @Override public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    loadedKey = PersistentCache.get()
+        .load(KEY_PRESENTER, savedInstanceState,
+            new PersistLoader.Callback<SettingsFragmentPresenter>() {
+
+              @NonNull @Override public PersistLoader<SettingsFragmentPresenter> createLoader() {
+                return new SettingsFragmentPresenterLoader();
+              }
+
+              @Override public void onPersistentLoaded(@NonNull SettingsFragmentPresenter persist) {
+                presenter = persist;
+              }
+            });
+  }
+
+  @Nullable @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    binding = FragmentMainBinding.inflate(inflater, container, false);
+    return binding.getRoot();
+  }
+
+  @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    setupFAB();
+  }
+
+  private void setupFAB() {
+    binding.mainSettingsFab.setOnClickListener(view -> presenter.clickFAB());
+    FABUtil.setupFABBehavior(binding.mainSettingsFab, new HideScrollFABBehavior(10));
+  }
+
+  @Override public void onDestroyView() {
+    super.onDestroyView();
+    AsyncMapHelper.unsubscribe(fabTask);
+    binding.unbind();
+  }
+
+  @Override public void onDestroy() {
+    super.onDestroy();
+    if (!getActivity().isChangingConfigurations()) {
+      PersistentCache.get().unload(loadedKey);
+    }
+    ZapTorch.getRefWatcher(this).watch(this);
+  }
+
+  @Override public void onSaveInstanceState(Bundle outState) {
+    PersistentCache.get()
+        .saveKey(outState, KEY_PRESENTER, loadedKey, SettingsFragmentPresenter.class);
+    super.onSaveInstanceState(outState);
+  }
+
+  @Override public void onResume() {
+    super.onResume();
+    setActionBarUpEnabled(false);
+    presenter.loadFABFromState();
+    displayPreferenceFragment();
+  }
+
+  @Override public void onStop() {
+    super.onStop();
+    presenter.unbindView();
+  }
+
+  @Override public void onStart() {
+    super.onStart();
+    presenter.bindView(this);
+  }
+
+  private void displayPreferenceFragment() {
+    final FragmentManager fragmentManager = getChildFragmentManager();
+    if (fragmentManager.findFragmentByTag(SettingsPreferenceFragment.TAG) == null) {
+      fragmentManager.beginTransaction()
+          .replace(R.id.main_container, new SettingsPreferenceFragment(),
+              SettingsPreferenceFragment.TAG)
+          .commit();
+    }
+  }
+
+  @Override public void onFABEnabled() {
+    AsyncMapHelper.unsubscribe(fabTask);
+    fabTask = AsyncDrawable.load(R.drawable.ic_help_24dp).into(binding.mainSettingsFab);
+  }
+
+  @Override public void onFABDisabled() {
+    AsyncMapHelper.unsubscribe(fabTask);
+    fabTask = AsyncDrawable.load(R.drawable.ic_service_start_24dp).into(binding.mainSettingsFab);
+  }
+
+  @Override public void onCreateAccessibilityDialog() {
+    AppUtil.guaranteeSingleDialogFragment(getFragmentManager(), new AccessibilityRequestDialog(),
+        "accessibility");
+  }
+
+  @Override public void onDisplayServiceInfo() {
+    AppUtil.guaranteeSingleDialogFragment(getFragmentManager(), new ServiceInfoDialog(),
+        "servce_info");
+  }
+}
