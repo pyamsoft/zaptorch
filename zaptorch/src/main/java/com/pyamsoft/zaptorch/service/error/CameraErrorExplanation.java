704f2fd03f54565f4c0e5ae80f225119aa5d7e17
pyamsoft <pyam.soft@gmail.com>
2016-12-19 23:11:28 -0800

    Split project into modules for view and presenter

---
+++ zaptorch/src/main/java/com/pyamsoft/zaptorch/service/error/CameraErrorExplanation.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2016 Peter Kenji Yamanaka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pyamsoft.zaptorch.service.error;
+
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import com.pyamsoft.pydroid.util.AppUtil;
+
+import static com.pyamsoft.zaptorchpresenter.service.CameraInterface.DIALOG_WHICH;
+import static com.pyamsoft.zaptorchpresenter.service.CameraInterface.TYPE_ERROR;
+import static com.pyamsoft.zaptorchpresenter.service.CameraInterface.TYPE_NONE;
+import static com.pyamsoft.zaptorchpresenter.service.CameraInterface.TYPE_PERMISSION;
+
+public class CameraErrorExplanation extends AppCompatActivity {
+
+  @Override protected void onPostResume() {
+    super.onPostResume();
+    final int type = getIntent().getIntExtra(DIALOG_WHICH, TYPE_NONE);
+    DialogFragment fragment;
+    switch (type) {
+      case TYPE_PERMISSION:
+        fragment = new PermissionErrorDialog();
+        break;
+      case TYPE_ERROR:
+        fragment = new CameraErrorDialog();
+        break;
+      default:
+        fragment = null;
+    }
+    if (fragment != null) {
+      AppUtil.guaranteeSingleDialogFragment(getSupportFragmentManager(), fragment, "error");
+    }
+  }
+}
---
+++ zaptorch/src/main/java/com/pyamsoft/zaptorch/settings/HowToDialog.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2016 Peter Kenji Yamanaka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pyamsoft.zaptorch.settings;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import com.pyamsoft.zaptorch.ZapTorch;
+
+public class HowToDialog extends DialogFragment {
+
+  @NonNull @Override public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+    return new AlertDialog.Builder(getActivity()).setMessage(
+        "Just double press the volume down key to turn the flashlight on and off")
+        .setTitle("How to Use")
+        .setNeutralButton("Got It", (dialog, which) -> dialog.dismiss())
+        .create();
+  }
+
+  @Override public void onDestroy() {
+    super.onDestroy();
+    ZapTorch.getRefWatcher(this).watch(this);
+  }
+}
