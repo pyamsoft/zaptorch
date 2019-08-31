b897fb11d6117964f27676190519914f662e73ac
pyamsoft <developer.pyamsoft@gmail.com>
2019-08-31 11:36:19 -0700

    Use MC

--- app/src/main/java/com/pyamsoft/zaptorch/main/MainToolbarView.kt
+++ app/src/main/java/com/pyamsoft/zaptorch/main/MainToolbarView.kt
@@ -59,9 +59,9 @@ internal class MainToolbarView @Inject internal constructor(
   private fun setupToolbar() {
     val theme: Int
     if (theming.isDarkTheme(requireNotNull(activity))) {
-      theme = R.style.ThemeOverlay_AppCompat
+      theme = R.style.ThemeOverlay_MaterialComponents
     } else {
-      theme = R.style.ThemeOverlay_AppCompat_Light
+      theme = R.style.ThemeOverlay_MaterialComponents_Light
     }
 
     layoutRoot.apply {
